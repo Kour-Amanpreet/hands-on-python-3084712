180157eafc4c1f66a7dbbdeeabf215fcae0ea45a
Kour-Amanpreet <amanpreetkour6259@gmail.com>
2024-05-24 09:28:34 +0000

    Pending changes exported from your codespace

--- Ex_Files/02_05_begin/main.py
+++ Ex_Files/02_05_begin/main.py
@@ -18,3 +18,14 @@ elif current_env in [CODE_SPACE, LOCAL]:
     print("Codespace or local environment")
 else:
     print("Unknown environment")
+
+
+""" I can change output by writing following command in the terminal:
+export ENV_NAME='code_space'          (say)
+then follwing output is recieved on typing command python main.py
+Codespace or local environment
+If I write some arbitrary environment name as: 
+export ENV_NAME='AKMAN'          (say)
+then follwing output is recieved on typing command python main.py
+Unknown environment
+"""
